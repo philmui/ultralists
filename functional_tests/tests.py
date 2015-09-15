6795dd6584cad80fbf9e846918e680ded0aadb95
Phil Mui <philmui@heartflow.com>
2015-09-14 23:14:09 -0700

    initial commit

---
+++ functional_tests/tests.py
@@ -0,0 +1,23 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+import unittest
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_simple(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Django', self.browser.title)
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
