31c4c689b7a39c14dc35397e952a867d0cee3740
Phil Mui <philmui@heartflow.com>
2015-09-26 12:23:22 -0700

    new dev branch

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,26 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 
 from lists.views import home_page
+from lists.models import Item
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "First ever list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Second item"
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved = saved_items[0]
+        second_saved = saved_items[1]
+        self.assertEqual(first_saved.text, first_item.text)
+        self.assertEqual(second_saved.text, second_item.text)
 
 class HomePageTest(TestCase):
 
