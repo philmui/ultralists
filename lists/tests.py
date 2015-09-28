ad6050e3b8f2bfd7b8f087bac5443a27043825d6
Phil Mui <philmui@heartflow.com>
2015-09-28 14:35:09 -0700

    Added new action: new list

--- lists/tests.py
+++ lists/tests.py
@@ -40,12 +40,25 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/the-only-list/')
         self.assertTemplateUsed(response, 'list.html')
 
-class HomePageTest(TestCase):
+class NewListTest(TestCase):
 
-    def test_home_page_only_saves_items_when_necessary(self):
-        request = HttpRequest()
-        home_page(request)
-        self.assertEqual(Item.objects.count(), 0)
+    def test_saving_a_POST_request(self):
+        self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'}
+        )
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+            '/lists/new',
+            data={'item_text': 'A new list item'}
+        )
+        self.assertRedirects(response, '/lists/the-only-list/')
+
+class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
@@ -57,6 +70,7 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
+    '''
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
         request.method = 'POST'
@@ -68,6 +82,11 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_home_page_redirects_after_POST(self):
         request = HttpRequest()
         request.method = 'POST'
@@ -77,7 +96,7 @@ class HomePageTest(TestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/lists/the-only-list/')
-    '''
+
     def test_home_page_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
--- lists/views.py
+++ lists/views.py
@@ -5,14 +5,19 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
+    '''
     if request.method == 'POST':
         item_text = request.POST['item_text']
         if (item_text != ''):
             Item.objects.create(text=item_text)
         return redirect("/lists/the-only-list/")
-
+    '''
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect("/lists/the-only-list/")
