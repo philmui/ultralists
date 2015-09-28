e61db45967e57fc2ca63b0bad27d5278784286c0
Phil Mui <philmui@heartflow.com>
2015-09-27 22:56:49 -0700

    A new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -9,7 +9,10 @@ def home_page(request):
         item_text = request.POST['item_text']
         if (item_text != ''):
             Item.objects.create(text=item_text)
-        return redirect("/")
+        return redirect("/lists/the-only-list/")
 
+    return render(request, 'home.html')
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
