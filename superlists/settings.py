08cd28bbc831edd36e35063b29db6d1c06722b25
Phil Mui <pmui@heartflow.com>
2015-10-12 03:32:03 +0000

    added nginx & gunicorn conf's

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm_*#^bd@^6=#_1p#72yccx%%!_)@$r72uc9v0#(3j=k*t_6&id'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# Needed when DEBUG=False
+ALLOWED_HOSTS = ['www.heartflow.us', 'heartflow.us', 'localhost']
 
 
 # Application definition
