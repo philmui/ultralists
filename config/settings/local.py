e059573ac188efd3b0d8b4344b6abd70b1aa34da
Phil Mui <thephilmui@gmail.com>
2015-10-19 22:32:53 -0700

    separated out base.py from local.py

---
+++ config/settings/local.py
@@ -0,0 +1,12 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+    }
+}
+
